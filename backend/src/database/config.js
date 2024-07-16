84b356e0619efd0a62c1a7bc9fb43048701aaff8
kirlian <rankirlian@gmail.com>
2024-07-17 01:46:32 +0200

    Fix on network setup to let apps properly communicate

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbName = 'prjmanager';
-const url = `mongodb://127.0.0.1:27017/${dbName}`;
+const url = `mongodb://localhost:27017/${dbName}`;
 
 const dbConnection = async () => {
 
