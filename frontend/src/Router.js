8ad478e17c7fbac1fb271be3052e597d063754ad
Abaranasia <rankirlian@gmail.com>
2024-07-19 00:17:19 +0200

    Update router

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
-import { Dashboard } from './views/Dashboard/Dashboard';
-import { Project } from './views/ProjectDetail/Project';
-import { ProjectList } from './views/ProjectList/ProjectList';
-import { Resource } from './views/ResourceDetail/Resource';
-import { Resources } from './views/Resources/Resources';
-import { AddProject } from './views/AddProject/AddProject';
-import { AddResource } from './views/AddResource/AddResource';
+import { Dashboard } from './views/';
+import { ProjectDetail } from './views/';
+import { ProjectList } from './views/';
+import { ResourceDetail } from './views/';
+import { Resources } from './views';
+import { AddProject } from './views';
+import { AddResource } from './views';
+import { Login } from './views';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} exact />
+      <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/projects" element={<ProjectList />} exact />
-      <Route path="/project" element={<Project />} exact />
+      <Route path="/project" element={<ProjectDetail />} exact />
       <Route path="/project/add" element={<AddProject />} exact />
       <Route path="/resources" element={<Resources />} exact />
-      <Route path="/resource" element={<Resource />} exact />
+      <Route path="/resource" element={<ResourceDetail />} exact />
       <Route path="/resource/add" element={<AddResource />} exact />
-      <Route path="*" element={<Dashboard />} exact />
+      <Route path="*" element={<p>There's nothing here: 404!</p>} />
     </Routes>
   )
 }
