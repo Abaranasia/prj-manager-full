029a40f7e49db55db451d64092f84a6235134a68
Abaranasia <rankirlian@gmail.com>
2024-07-19 00:58:02 +0200

    Add ProtectedRoute to all views except Login

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -9,19 +9,84 @@ import { Resources } from './views';
 import { AddProject } from './views';
 import { AddResource } from './views';
 import { Login } from './views';
+import ProtectedRoute from './ui/ProtectedRoute';
+
+let user;
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} exact />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/projects" element={<ProjectList />} exact />
-      <Route path="/project" element={<ProjectDetail />} exact />
-      <Route path="/project/add" element={<AddProject />} exact />
-      <Route path="/resources" element={<Resources />} exact />
-      <Route path="/resource" element={<ResourceDetail />} exact />
-      <Route path="/resource/add" element={<AddResource />} exact />
+      <Route path="/" element={<Login />} />
+      
+      <Route 
+        path="/dashboard" 
+        element={
+          <ProtectedRoute user={user}>
+            <Dashboard />
+          </ProtectedRoute>
+        } 
+      />
+
+      <Route 
+        path="/projects" 
+        element={
+          <ProtectedRoute user={user}>
+            <ProjectList />
+          </ProtectedRoute>
+        } 
+        exact 
+      />
+
+      <Route 
+        path="/project" 
+        element={
+          <ProtectedRoute user={user}>
+            <ProjectDetail />
+          </ProtectedRoute>
+        } 
+        exact 
+      />
+      
+      <Route 
+        path="/project/add" 
+        element={
+          <ProtectedRoute user={user}>
+            <AddProject />
+          </ProtectedRoute>
+        } 
+        exact
+        />
+
+      <Route 
+        path="/resources" 
+        element={
+          <ProtectedRoute user={user}>
+            <Resources />
+          </ProtectedRoute>
+          } 
+        exact 
+      />
+
+      <Route 
+        path="/resource" 
+        element={
+          <ProtectedRoute user={user}>
+            <ResourceDetail />
+          </ProtectedRoute>
+          } 
+        exact 
+      />
+
+      <Route 
+        path="/resource/add"
+        element={
+          <ProtectedRoute user={user}>
+            <AddResource />
+          </ProtectedRoute>
+          } 
+        exact 
+       />
+
       <Route path="*" element={<p>There's nothing here: 404!</p>} />
     </Routes>
   )
